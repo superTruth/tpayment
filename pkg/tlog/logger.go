1038259442027b6d7fd97aa3e3f048c4161efc9c
Truth <qiang.fang@bindo.com>
2020-09-04 15:30:54 +0800

    修复日志问题

--- pkg/tlog/logger.go
+++ pkg/tlog/logger.go
@@ -8,37 +8,6 @@ import (
 	"go.uber.org/zap"
 )
 
-//type Logger struct {
-//	logger *zap.Logger
-//	sugar  *zap.SugaredLogger
-//	tag    string
-//}
-//
-//func (this *Logger) Init(tag string) {
-//	this.tag = tag
-//	this.logger, _ = zap.NewProduction()
-//	this.logger = this.logger.With(zap.String(conf.HeaderTagRequestId, this.tag))
-//	this.sugar = this.logger.Sugar()
-//}
-//
-//func (this *Logger) SetTag(key, value string) {
-//	this.logger = this.logger.With(zap.String(key, value))
-//	this.sugar = this.logger.Sugar()
-//}
-//
-//
-//func (this *Logger) Info(args ...interface{}) {
-//	this.sugar.Info(args...)
-//}
-//
-//func (this *Logger) Warn(args ...interface{}) {
-//	this.sugar.Warn(args...)
-//}
-//
-//func (this *Logger) Error(args ...interface{}) {
-//	this.sugar.Error(args...)
-//}
-//
 func SetLogger(ctx *gin.Context, logger *Logger) {
 	ctx.Set(conf.ContextTagLog, logger)
 }
@@ -54,6 +23,7 @@ func GetLogger(ctx *gin.Context) *Logger {
 	log := NewLog("")
 
 	return log
+
 }
 
 var once sync.Once
@@ -68,10 +38,10 @@ func NewLog(tag string) *Logger {
 	once.Do(func() {
 		logger, _ = zap.NewProduction()
 	})
-	logger = logger.With(zap.String(conf.HeaderTagRequestId, tag))
+	tmpLogger := logger.With(zap.String(conf.HeaderTagRequestId, tag))
 
 	return &Logger{
-		SugaredLogger: logger.Sugar(),
+		SugaredLogger: tmpLogger.Sugar(),
 	}
 }
 
